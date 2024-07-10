90d45fad33c64ad16b8e818b82470d17e6a689cd
Muskankothari24 <157019154+Muskankothari24@users.noreply.github.com>
2024-07-11 00:49:43 +0530

    Update ok.js

--- ok.js
+++ ok.js
@@ -10,8 +10,8 @@ arrows.forEach((arrow, i) => {
   arrow.addEventListener("click", () => {
     const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
-    // if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      if (itemNumber - (4 + clickCounter)  >= 1) {
+     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+     // if (itemNumber - (4 + clickCounter)  >= 1) {
       mLists[i].style.transform = `translateX(${
         mLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
